338f906537e4d26a5f490ab37bfc96bf234d83c3
Sandra Peterson <writesandy@gmail.com>
2018-05-11 22:24:48 -0500

    on click

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 // data in an array of friends
 
-let friends =
-{
-    "name": "Sandra",
-    "photo": "https://www.facebook.com/photo.php?fbid=10157415563609676&l=e4bde4ee99",
-    "scores": [
-        5,
-        5,
-        2,
-        1,
-        3,
-        4,
-        5,
-        3,
-        5,
-        2,
-    ]
-}
+let friends = [
+    {
+        "name": "Sandra",
+        "photo": "https://www.facebook.com/photo.php?fbid=10157415563609676&l=e4bde4ee99",
+        "scores": [
+            5,
+            5,
+            2,
+            1,
+            3,
+            4,
+            5,
+            3,
+            5,
+            2,
+        ]
+    }]
--- server.js
+++ server.js
@@ -69,11 +69,8 @@ app.get("/api/friends/:friends", function(req, res) {
 app.post("/api/friends", function(req, res) {
 
     var newfriend = req.body;
-
     console.log(newfriend);
-
     friends.push(newfriend);
-
     res.json(newfriend);
 
 });

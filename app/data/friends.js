c3da36b4f8217d55cfb302d392e1724f4afe5038
Sandra Peterson <writesandy@gmail.com>
2018-05-14 21:14:21 -0500

    more data

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,7 +17,22 @@ let friends = [
             "2",
         ]
     },
-
+    {
+        "name": "Fred Flintstone",
+        "photo": "http://wp.production.patheos.com/blogs/brotherrichard/files/2015/06/Fred.jpg",
+        "scores": [
+            "1",
+            "3",
+            "4",
+            "1",
+            "3",
+            "2",
+            "1",
+            "5",
+            "1",
+            "1",
+        ]
+    },
     {  
          "name": "Mark Peterson",
         "photo": "https://www.facebook.com/photo.php?fbid=10151191323854676&l=7b9d35d9f4",
@@ -35,8 +50,25 @@ let friends = [
         ]
         },
 
+        {
+            "name": "Barny",
+            "photo": "https://i.ytimg.com/vi/YMJvK4XTZhY/maxresdefault.jpg",
+            "scores": [
+            "5",
+            "3",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "5",
+            "4",
+            "1"
+            ]
+            },
+
         { name: 'Jeremy',
-        photo: 'https://www.facebook.com/photo.php?fbid=10152510192084676&set=t.548134675&type=3&theater',
+        photo: 'https://www.askideas.com/media/37/Man-With-Horse-Hairstyle-Funny-Image-For-Whatsapp.jpg',
         scores: [ '1', '1', '1', '1', '2', '1', '3', '1', '1', '1' ] },
     ]
     module.exports = friends
